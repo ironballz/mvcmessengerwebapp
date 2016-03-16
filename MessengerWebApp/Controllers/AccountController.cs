98f3aa3510b7798032b614dea2b9218d3bbeadf1
ironballz <dimitry.bespalchuk@gmail.com>
2016-03-17 01:33:07 +0200

    Account viewmodels, controller and registration view has been added.

---
+++ MessengerWebApp/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MessengerWebApp.ViewModels;
+
+namespace MessengerWebApp.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Register(RegisterViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View(user);
+        }
+
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel credentials)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View(credentials);
+        }
+    }
+}
---
+++ MessengerWebApp/ViewModels/AccountViewModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MessengerWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "{0} is required.")]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
+        [EmailAddress(ErrorMessage = "{0} field does not contain valid email address.")]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(20, ErrorMessage = "{0} must be at least {2} and shorter than {1} characters long .", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
